e2c6b7d5618f51e4b0754d77582f8a2a10f3c0ca
Laura <71552624+licsalomon@users.noreply.github.com>
2021-04-17 12:29:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 /* ****************** Aqui va la logica principal del servidor que usará express,
 debe levantarse escuchando peticiones en el puerto 8080 ****************** */
+const express = require('express');
+const app = express();
+
+app.get('/');
+
+app.listen('8080', function() {
+  console.log('texto para probar Servidor web escuchando en el puerto 8080');
+});
