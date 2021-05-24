e8b1c522ef794e15ce6fe51a434d7967c6131932
Laura <71552624+licsalomon@users.noreply.github.com>
2021-05-24 12:12:01 -0300

    Agregué rutas al server

--- server.js
+++ server.js
@@ -8,3 +8,32 @@ app.get('/');
 app.listen('8080', function() {
   console.log('texto para probar Servidor web escuchando en el puerto 8080');
 });
+
+
+
+//server con rutas
+
+const express = require('express');
+const app = express();
+
+
+app.get('/' , function( peticion, respuesta){
+   
+    respuesta.send("ruta funcionando")
+  })
+
+  var adminRouter = express.Router();
+
+  adminRouter.get('/', function(req,res){
+    res.send("index");
+  })
+
+  adminRouter.get('/random', function(req, res){
+    res.send("random page");
+  })
+
+
+app.listen('8080', function() {
+  console.log('texto para probar Servidor web escuchando en el puerto 8080');
+});
+
